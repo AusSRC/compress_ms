21aaff84fcf10e3885f2bdc10e288c45b2042ec9
Alex Williamson <alxndrwllmsn@gmail.com>
2025-10-02 10:28:42 +0800

    Added output to stdout during runtime, added exception handling

--- compress_ms.cc
+++ compress_ms.cc
@@ -30,7 +30,25 @@ int main (int argc, const char* argv[])
         errBound = argv[4];
         errBoundType = argv[5]; 
     }
+        std::cout << "Parameters received: \n \
+            \tinput_ms: " + inFile + "\n \
+            \toutput_ms: " + outFile + "\n \
+            \tcolumn_name: " + colName + "\n \
+            \terror_bound: " + errBound + "\n \
+            \tABS/REL: " + errBoundType << std::endl;
 
+    if ( errBoundType != "ABS" && errBoundType != "REL")
+    {
+        throw std::invalid_argument("Ensure that the <ABS/REL> parameter is either ABS or REL");
+    }
+    try 
+    {
+        std::cout << "The error bound parsed as a number is " + std::to_string(std::stof(errBound)) << std::endl;
+    }
+    catch (const std::invalid_argument& ex)
+    {
+        throw std::invalid_argument("Error bound must be a number.");
+    }
     {
         //define adios storage manager
         Adios2StMan adios2stman(
@@ -42,11 +60,14 @@ int main (int argc, const char* argv[])
             {"Accuracy", errBound},
             {"Mode", errBoundType}}});
         
+        std::cout << "Opening MeasurementSet" << std::endl;
+
         MeasurementSet msIn(inFile);
         ArrayColumn<Complex> dataCol(msIn, colName);
         TableDesc msTD = msIn.tableDesc();
         SetupNewTable newTab(outFile, msTD, Table::New);
         
+        std::cout << "Starting Copy" << std::endl;
         //copy measurement set
         newTab.bindColumn(colName, adios2stman);
         MeasurementSet msOut(newTab);
@@ -56,11 +77,13 @@ int main (int argc, const char* argv[])
         for (uInt i=0; i<msTD.ncolumn(); i++)
         {
             std::string colName_i = msTD.columnDesc(i).name();
+            std::cout << "Copying Column: " + colName_i << std::endl;
             TableCopy::copyColumnData(msIn, colName_i, msOut, colName_i, false);
         }   
     }
 
     //check output
     // MPI_Finalize();
+    std::cout << "Done" << std::endl;
     return 0;
 }
