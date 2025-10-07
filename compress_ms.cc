329d3212db8ef768013760b5998fd9dd924e8687
Alex Williamson <alxndrwllmsn@gmail.com>
2025-10-07 09:07:25 +0800

    jic

--- compress_ms.cc
+++ compress_ms.cc
@@ -16,60 +16,90 @@ int main (int argc, const char* argv[])
         //column name
         //compression error bound
         //ABS/REL error bound
-    std::string inFile, outFile, colName, errBound, errBoundType;
-    if ( argc != 6 ) 
+    std::string inFile, outFile, colName, errBound, errBoundType, configFile;
+    if ( argc == 6 ) 
     {
-        std::cout << "Usage: compress_ms <input_ms> <output_ms> <column_name> <error_bound> <ABS/REL>" << std:: endl;
-        return -1;
-    }
-    else
-    {
+        std::cout << "Assuming manual parameter entries" << std::endl;
         inFile = argv[1];
         outFile = argv[2];
         colName = argv[3];
         errBound = argv[4];
         errBoundType = argv[5]; 
-    }
-        std::cout << "Parameters received: \n \
-            \tinput_ms: " + inFile + "\n \
-            \toutput_ms: " + outFile + "\n \
-            \tcolumn_name: " + colName + "\n \
-            \terror_bound: " + errBound + "\n \
-            \tABS/REL: " + errBoundType << std::endl;
 
-    if ( errBoundType != "ABS" && errBoundType != "REL")
-    {
-        throw std::invalid_argument("Ensure that the <ABS/REL> parameter is either ABS or REL");
+        std::cout << "Parameters received: \n \
+        \tinput_ms: " + inFile + "\n \
+        \toutput_ms: " + outFile + "\n \
+        \tcolumn_name: " + colName + "\n \
+        \terror_bound: " + errBound + "\n \
+        \tABS/REL: " + errBoundType << std::endl;
+
+        if ( errBoundType != "ABS" && errBoundType != "REL")
+        {
+            throw std::invalid_argument("Ensure that the <ABS/REL> parameter is either ABS or REL");
+        }
+        try 
+        {
+            std::cout << "The error bound parsed as a number is " + std::to_string(std::stof(errBound)) << std::endl;
+        }
+        catch (const std::invalid_argument& ex)
+        {
+            throw std::invalid_argument("Error bound must be a number.");
+        }
     }
-    try 
+    else if ( argc == 5)
     {
-        std::cout << "The error bound parsed as a number is " + std::to_string(std::stof(errBound)) << std::endl;
+        std::cout << "Assuming config file entry" << std::endl;
+        inFile = argv[1];
+        outFile = argv[2];
+        colName = argv[3];
+        configFile = argv[4];
+
+        std::cout << "Parameters received: \n \
+        \tinput_ms: " + inFile + "\n \
+        \toutput_ms: " + outFile + "\n \
+        \tcolumn_name: " + colName + "\n \
+        \tconfig_file: " + configFile << std::endl;
     }
-    catch (const std::invalid_argument& ex)
+    else
     {
-        throw std::invalid_argument("Error bound must be a number.");
+        std::cout << "Usage: compress_ms <input_ms> <output_ms> <column_name> <error_bound> <ABS/REL>\n \
+        \tcompress_ms <input_ms> <output_ms> <column_name> <config_file>" << std:: endl;
+        return -1;
     }
+    
     {
         //define adios storage manager
-        Adios2StMan adios2stman(
-            std::string(""),
-            {},
-            {{}},
-            {{{"Variable", colName},
-            {"Operator", "mgard"},
-            {"Accuracy", errBound},
-            {"Mode", errBoundType}}});
+        
         
         std::cout << "Opening MeasurementSet" << std::endl;
 
         MeasurementSet msIn(inFile);
-        ArrayColumn<Complex> dataCol(msIn, colName);
+        ArrayColumn<float> dataCol(msIn, colName);
         TableDesc msTD = msIn.tableDesc();
         SetupNewTable newTab(outFile, msTD, Table::New);
         
         std::cout << "Starting Copy" << std::endl;
         //copy measurement set
-        newTab.bindColumn(colName, adios2stman);
+        if (configFile.empty()) 
+        {
+            Adios2StMan adios2stman(
+                std::string(""),
+                {},
+                {{}},
+                {{{"Variable", colName},
+                {"Operator", "mgard"},
+                {"Accuracy", errBound},
+                {"Mode", errBoundType}}});
+            newTab.bindColumn(colName, adios2stman);
+        }
+        else
+        {
+            Adios2StMan::from_config_t from_config {};
+            Adios2StMan adios2stman(
+                configFile,
+                from_config);
+            newTab.bindColumn(colName, adios2stman);
+        }
         MeasurementSet msOut(newTab);
         TableCopy::copySubTables(msOut, msIn);
         msOut.addRow(msIn.nrow());
